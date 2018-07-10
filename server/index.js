deb8d136e8aa927735d5ba8add709357a9fed140
Tim Biles <timbilestimbiles@gmail.com>
2018-07-10 15:03:04 -0500

    complete

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const port = 3000;
 
+const checkForSession = require('./middlewares/checkForSession');
+const swag_controller = require('./controllers/swag_controller');
+const auth_controller = require('./controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
+const search_controller = require('./controllers/search_controller');
+
 const app = express();
 
 app.use(bodyParser());
@@ -15,7 +21,25 @@ app.use(
   })
 );
 
-app.use(express());
+app.use(checkForSession);
+app.use(express.static(`${__dirname}/build`));
+
+//swag
+app.get('/api/swag', swag_controller.read);
+
+//auth
+app.post('/api/login', auth_controller.login);
+app.post('/api/register', auth_controller.register);
+app.post('/api/signout', auth_controller.signout);
+app.get('/api/user', auth_controller.getUser);
+
+//cart
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
+
+//search
+app.get('/api/search', search_controller.search);
 
 app.listen(port, () => {
   console.log(`Listening on Port${port}`);
